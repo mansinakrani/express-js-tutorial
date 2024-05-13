445b6124ceea87531ecd993b837290ec5d28cf5d
mansinakrani <mansinakrani006@gmail.com>
2024-05-13 16:31:32 +0530

    feat: route params

--- src/index.mjs
+++ src/index.mjs
@@ -4,17 +4,30 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+const mockUsers = [
+  { id:1, username: 'anson', displayname: 'Anson' },
+  { id:2, username: 'jack', displayname: 'Jack' },
+  { id:3, username: 'adam', displayname: 'Adam' }
+];
+
 app.get('/', (request, response) => {
   // response.send("Hello, World!");
   response.status(201).send({ msg: "Hello!" })
 });
 
 app.get('/api/users', (request, response) => {
-  response.send([
-    { id:1, username: 'anson', displayname: 'Anson' },
-    { id:2, username: 'jack', displayname: 'Jack' },
-    { id:3, username: 'adam', displayname: 'Adam' }
-  ]);
+  response.send(mockUsers);
+});
+
+app.get("/api/users/:id", (request, response) => {
+	console.log(request.params);
+	const parsedId = parseInt(request.params.id);
+	if (isNaN(parsedId)) 
+  return response.status(400).send({ msg: "Bad Request. Invalid ID." });
+
+	const findUser = mockUsers.find((user) => user.id === parsedId);
+	if (!findUser) return response.sendStatus(404);
+	return response.send(findUser);
 });
 
 app.get("/api/products", (request, response) => {
