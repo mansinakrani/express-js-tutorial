fb3f8c1c675c445c793c0d3e5ae055d70009fdf4
mansinakrani <mansinakrani006@gmail.com>
2024-05-13 15:21:41 +0530

    feat: get requests

--- src/index.mjs
+++ src/index.mjs
@@ -4,6 +4,24 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.get('/', (request, response) => {
+  // response.send("Hello, World!");
+  response.status(201).send({ msg: "Hello!" })
+});
+
+app.get('/api/users', (request, response) => {
+  response.send([
+    { id:1, username: 'anson', displayname: 'Anson' },
+    { id:2, username: 'jack', displayname: 'Jack' },
+    { id:3, username: 'adam', displayname: 'Adam' }
+  ]);
+});
+
+app.get("/api/products", (request, response) => {
+	response.send([{ id: 123, name: "Rice", price: 12.99 }]);
+});
+
 app.listen(PORT, () =>  {
-  console.log(`Runnning on port ${PORT}`)
+  console.log(`Runnning on port ${PORT}`);
 })
+;
