b5200ea326eb13dec9fed4a3756a0976c80cfdf1
mansinakrani <mansinakrani006@gmail.com>
2024-05-14 15:35:47 +0530

    feat: query  params

--- src/index.mjs
+++ src/index.mjs
@@ -7,7 +7,11 @@ const PORT = process.env.PORT || 3000;
 const mockUsers = [
   { id:1, username: 'anson', displayname: 'Anson' },
   { id:2, username: 'jack', displayname: 'Jack' },
-  { id:3, username: 'adam', displayname: 'Adam' }
+  { id:3, username: 'adam', displayname: 'Adam' },
+  { id:4, username: "tina", displayName: "Tina" },
+	{ id:5, username: "jason", displayName: "Jason" },
+	{ id:6, username: "henry", displayName: "Henry" },
+	{ id:7, username: "marilyn", displayName: "Marilyn" },
 ];
 
 app.get('/', (request, response) => {
@@ -16,7 +20,15 @@ app.get('/', (request, response) => {
 });
 
 app.get('/api/users', (request, response) => {
-  response.send(mockUsers);
+  console.log(request.query);
+  // response.send(mockUsers);
+  const { query: { filter, value}, } = request;
+
+	// when filter & value are undefined
+	if (filter && value) return response.send(
+		mockUsers.filter((user) => user[filter].includes(value))
+		);
+		return response.send(mockUsers);
 });
 
 app.get("/api/users/:id", (request, response) => {
@@ -36,5 +48,4 @@ app.get("/api/products", (request, response) => {
 
 app.listen(PORT, () =>  {
   console.log(`Runnning on port ${PORT}`);
-})
-;
+});
